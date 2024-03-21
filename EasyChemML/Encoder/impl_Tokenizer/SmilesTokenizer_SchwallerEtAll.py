2b099a7e0aa69e5618410c15d7d2ab27810f802a
l_schl60 <l_schl60@uni-muenster.de>
2024-03-21 14:53:04 +0100

    bug fix tokenizer

--- EasyChemML/Encoder/impl_Tokenizer/SmilesTokenizer_SchwallerEtAll.py
+++ EasyChemML/Encoder/impl_Tokenizer/SmilesTokenizer_SchwallerEtAll.py
@@ -227,28 +227,7 @@ class SmilesTokenzier():
         out_tokens = self.decode_ids_to_tokens(ID_output)
         return self.decode_tokens_to_seperate_outputs(out_tokens)
 
-"""
-    def getSmilesfromoutputwithSD(self, ID_output: torch.Tensor) -> Tuple[str, str, np.ndarray]:
-
-        ID_output = np.array(ID_output.cpu())
-        decoded = self.decode(ID_output)
-        notokens = decoded.replace('[CLS]', '').replace('[SEP]', '').replace('[PAD]', '').strip().replace(' ', '')
-
-        splited_notokens = self._splitSMILES(notokens)
-
-        smistring = ''
-        densitystring = ''
-        DensityArray = []
-        for tok in splited_notokens:
-            if '_' not in tok:
-                smistring += tok
-            if '_' in tok:
-                DensityArray.append(int(tok.strip('_')))
-            densitystring += tok
-        return smistring, densitystring, np.array(DensityArray)
-"""
     def getSmilesfromoutputwithSD(self, ID_output: torch.Tensor) -> Tuple[str, str, np.ndarray, float]:
-
         ID_output = np.array(ID_output.cpu())
         decoded = self.decode(ID_output)
         notokens = decoded.replace('[CLS]', '').replace('[SEP]', '').replace('[PAD]', '').strip().replace(' ', '')
